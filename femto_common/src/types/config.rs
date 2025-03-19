3aa53cc4472c65f11681aad9fb963c77997b0ec3
Dax Harris <me@dax.gay>
2025-03-19 17:35:14 -0400

    more config stuffs

--- femto_common/src/types/config.rs
+++ femto_common/src/types/config.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
@@ -11,23 +13,198 @@ pub struct TemplateMetadata {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum NumberType {
+    #[serde(alias = "float")]
+    SignedFloat,
+
+    #[serde(alias = "+float")]
+    UnsignedFloat,
+
+    #[serde(alias = "int")]
+    SignedInt,
+
+    #[serde(alias = "+int")]
+    UnsignedInt,
+}
+
+impl Default for NumberType {
+    fn default() -> Self {
+        Self::SignedFloat
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "snake_case", tag = "type")]
 pub enum TemplateField {
     Text {
         key: String,
         prompt: String,
+        #[serde(default)]
         help: Option<String>,
+
+        #[serde(default)]
+        default: Option<String>,
+
+        #[serde(default)]
+        placeholder: Option<String>,
+    },
+    Password {
+        key: String,
+        prompt: String,
+        #[serde(default)]
+        help: Option<String>,
+
+        #[serde(default)]
+        default: Option<String>,
+
+        #[serde(default)]
+        placeholder: Option<String>,
     },
     Number {
         key: String,
         prompt: String,
+
+        #[serde(default)]
         help: Option<String>,
+
+        #[serde(default)]
+        default: Option<f64>,
+
+        #[serde(default)]
+        mode: NumberType,
+
+        #[serde(default)]
+        min: Option<f64>,
+
+        #[serde(default)]
+        max: Option<f64>,
     },
-    Confirm {
+    Boolean {
         key: String,
         prompt: String,
+
+        #[serde(default)]
         help: Option<String>,
+
+        #[serde(default)]
+        default: bool,
     },
+    Select {
+        key: String,
+        prompt: String,
+        options: Vec<String>,
+
+        #[serde(default)]
+        help: Option<String>,
+
+        #[serde(default)]
+        default: Option<String>,
+    },
+    SelectMultiple {
+        key: String,
+        prompt: String,
+        options: Vec<String>,
+
+        #[serde(default)]
+        help: Option<String>,
+
+        #[serde(default)]
+        default: Option<Vec<String>>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TemplateCommon {
+    key: String,
+    prompt: String,
+
+    #[serde(default)]
+    help: Option<String>,
+}
+
+impl TemplateField {
+    pub fn common(&self) -> TemplateCommon {
+        match self {
+            Self::Text {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            },
+            Self::Password {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            },
+            Self::Number {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            },
+            Self::Boolean {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            },
+            Self::Select {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            },
+            Self::SelectMultiple {
+                key, prompt, help, ..
+            } => TemplateCommon {
+                key: key.to_string(),
+                prompt: prompt.to_string(),
+                help: help.clone(),
+            }
+        }
+    }
+
+    pub fn key(&self) -> String {
+        self.common().key.clone()
+    }
+
+    pub fn prompt(&self) -> String {
+        self.common().prompt.clone()
+    }
+
+    pub fn help(&self) -> Option<String> {
+        self.common().help.clone()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TemplateStage {
+    pub key: String,
+    pub name: String,
+    pub fields: Vec<TemplateField>,
+    
+    #[serde(default)]
+    pub description: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TemplatePackageManager {
+    pub name: String,
+    pub path: String,
+    
+    #[serde(alias = "add")]
+    pub add_package: Vec<String>,
+
+    #[serde(alias = "remove")]
+    pub remove_package: Vec<String>,
+    pub help: Vec<String>,
+    pub other_commands: HashMap<String, (String, Vec<String>)>
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -35,4 +212,6 @@ pub struct TemplateConfig {
     pub slug: String,
     pub name: String,
     pub metadata: Option<TemplateMetadata>,
+    pub stages: Vec<TemplateStage>,
+    pub package_managers: Vec<TemplatePackageManager>
 }
